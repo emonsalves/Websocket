ed236d5abf9483f9bbeae88a872652d8ab4c6d76
Emonsalves <ingeniero.monsalves@gmail.com>
2023-06-16 23:55:08 -0400

    Feat : frontend socket.io client

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import io from 'socket.io-client'
+import { useState } from 'react'
+
+// const socket = io('http://localhost:3000')
+const socket = io('/')
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+export default App
